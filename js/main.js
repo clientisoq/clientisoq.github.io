f1492c11d20266cc740913dd066286356b65e22a
clientisoq <clientisoq@gmail.com>
2019-12-11 18:56:52 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const gallery = document.getElementById('gallery');
-
 // Initialization
 window.addEventListener('load', function() {
     var maxHeight = -1;
@@ -8,11 +6,23 @@ window.addEventListener('load', function() {
         maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
         $('.wall').height(maxHeight);
     });
-
 });
+
 
 // Grid
 $('.wall').jaliswall({
     item: '.wall-item',
     columnClass: '.wall-column'
+});
+
+
+// Filtering
+$('.filter-menu ul li').click(function() {
+    $('.filter-menu ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+
+    $('.wall-item').addClass('hidden');
+    $(selector).removeClass('hidden');    
 });
