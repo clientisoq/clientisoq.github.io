37063bf290064ba2ccb0256c8a10c08d05507e49
clientisoq <clientisoq@gmail.com>
2019-12-12 12:53:51 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Initialization
-window.addEventListener('load', function() {
+function SetGallerySize() {
     var maxHeight = -1;
 
     $('.wall-column').each(function() {
         maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
         $('.wall').height(maxHeight);
     });
-});
+}
+
+window.addEventListener('load', SetGallerySize);
+window.addEventListener('resize', SetGallerySize);
 
 
 // Grid
@@ -17,12 +20,40 @@ $('.wall').jaliswall({
 
 
 // Filtering
-$('.filter-menu ul li').click(function() {
-    $('.filter-menu ul li').removeClass('active');
+var filters = $('.filter-menu ul li');
+
+filters.click(function() {  
+    filters.removeClass('active');
     $(this).addClass('active');
 
-    var selector = $(this).attr('data-filter');
+    var wantedClassName = $(this).attr('data-filter');
+    var wall = $('.wall');
+    var wallItems = $('.wall-item');
+    var wantedItems = $(wantedClassName);
+    var unwantedItems = wallItems.not(wantedItems);
 
-    $('.wall-item').addClass('hidden');
-    $(selector).removeClass('hidden');    
+    wall.addClass('hideme');
+
+    setTimeout(() => {
+        wall.removeClass('hideme');
+        wantedItems.removeClass('hidden');
+        unwantedItems.addClass('vis-hidden');
+
+        setTimeout(function () {
+            wantedItems.removeClass('vis-hidden');
+            unwantedItems.addClass('hidden');
+            SetGallerySize();
+        }, 20);
+    }, 1000);
+});
+
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
 });
