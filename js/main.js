705f8af93ce35b727ea468bcd3f9a7b00832f09e
clientisoq <clientisoq@gmail.com>
2019-12-12 21:12:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,48 +12,42 @@ window.addEventListener('load', SetGallerySize);
 window.addEventListener('resize', SetGallerySize);
 
 
-// Grid
-$('.wall').jaliswall({
-    item: '.wall-item',
-    columnClass: '.wall-column'
+$( window ).on( "load", function() {
+    // Grid
+    $('.wall').jaliswall({
+        item: '.wall-item',
+        columnClass: '.wall-column'
+    });
+
+    // Filtering
+    var filters = $('.filter-menu ul li');
+
+    filters.click(function() {  
+        filters.removeClass('active');
+        $(this).addClass('active');
+
+        var wantedClassName = $(this).attr('data-filter');
+        var wall = $('.wall');
+        var wallItems = $('.wall-item');
+        var wantedItems = $(wantedClassName);
+        var unwantedItems = wallItems.not(wantedItems);
+
+        wall.addClass('hideme');
+
+        setTimeout(() => {
+            wall.removeClass('hideme');
+            wantedItems.removeClass('hidden');
+            unwantedItems.addClass('vis-hidden');
+
+            setTimeout(function () {
+                wantedItems.removeClass('vis-hidden');
+                unwantedItems.addClass('hidden');
+                SetGallerySize();
+            }, 20);
+        }, 1000);
+    });
 });
 
 
-// Filtering
-var filters = $('.filter-menu ul li');
-
-filters.click(function() {  
-    filters.removeClass('active');
-    $(this).addClass('active');
-
-    var wantedClassName = $(this).attr('data-filter');
-    var wall = $('.wall');
-    var wallItems = $('.wall-item');
-    var wantedItems = $(wantedClassName);
-    var unwantedItems = wallItems.not(wantedItems);
-
-    wall.addClass('hideme');
-
-    setTimeout(() => {
-        wall.removeClass('hideme');
-        wantedItems.removeClass('hidden');
-        unwantedItems.addClass('vis-hidden');
-
-        setTimeout(function () {
-            wantedItems.removeClass('vis-hidden');
-            unwantedItems.addClass('hidden');
-            SetGallerySize();
-        }, 20);
-    }, 1000);
-});
 
 
-$("a[href^='#']").click(function(e) {
-	e.preventDefault();
-	
-	var position = $($(this).attr("href")).offset().top;
-
-	$("body, html").animate({
-		scrollTop: position
-	} /* speed */ );
-});
